74f9d39356f3add01d01a60929fa14568628892c
AshtonPhillips2005 <ashtongamer11@gmail.com>
2024-09-19 16:39:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let studentName = "Ashton Phillips";//Makeing a string variable.
+console.log(studentName);//Should print Ashton Phillips into the console.
+
+let studentAge = "19";//Making a numeric variable.
+console.log(studentAge);//Should print 19 into the console.
+
+let isEnrolled = true;//Making a boolean variable.
+console.log(isEnrolled);//Should print true into the console.
+
+//Examples of the 5 JavaScript data types:
+
+//Example of string variable.
+let lastName = "Phillips";
+console.log(lastName);//Should print Phillips into the console.
+
+//Example of numeric variable. 
+let favouriteNumber = "10";
+console.log(favouriteNumber);//Should print 10 into the console.
+
+//Example of boolean variable.
+let isMale = true;
+console.log(isMale);//Should print true into the console.
+
+//Example of a array.
+let fruit = ['Banana','Orange','Apple'];
+console.log(fruit);//Should print banana, orange, and apple into the console.
+
+//Example of an object.
+let nintendoSwitch = {
+    month:"March",
+    day:"3",
+    year:"2017",
+};
+console.log(nintendoSwitch);//Should print March 3 2017 into the console.
+
+console.log(10 + 5);//Addition
+console.log(5 - 2);//Subtraction
+console.log(7 * 5);//Multiplication
+console.log(8 / 4);//Division
+
+let x = 12
+let y = 15
+
+console.log(x < y);//Should print true into the console.
+console.log(x > y);//Should print false into the console.
+console.log(x === y);//Should print false into the console.
+console.log(x !== y);//Should print true into the console.
+
+let t = true;
+let f = false;
+
+console.log(!f);//Logical NOT
+console.log(t && f);//Logical AND
+console.log(t || f);//Logical OR
+
+
